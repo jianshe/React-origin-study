dd6203d245786386fbef5c8172ce44529b20520f
jianshe <zhangshi789wei@163.com>
2019-10-28 01:03:34 +0800

    add study files

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var dataList =  {
+    friends: {
+        name: '好友',
+        list: [
+            {username:'八华就业',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'多情的水',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'八华就业',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'声威大震要',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'八华就业',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'城市的心',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'多情的雨',message:'什么叫人䚮，人才压顶地有城',vip:true}
+        ]
+        
+    },
+    families: {
+        name: '家人',
+        list: [
+            {username:'人之初',message:'天地倚仗，过客匆匆',vip:true},
+            {username:'性本善',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'性相近',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'习相远',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'苟不教',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'性乃迁',message:'什么叫人䚮，人才压顶地有城',vip:false}
+        ]
+    },
+    colleagues: {
+        name: '同事',
+        list: [
+            {username:'窦燕山',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'有义方',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'教五子',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'名俱扬',message:'什么叫人䚮，人才压顶地有城',vip:false},
+            {username:'苟不教',message:'什么叫人䚮，人才压顶地有城',vip:true},
+            {username:'举不教',message:'什么叫人䚮，人才压顶地有城',vip:false}
+        ]
+    }
+}
